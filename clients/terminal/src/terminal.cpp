a0778ac229f17884a3092e57eb37dbc358e1a9e0
Belkevich <aleksey.belkevich@gmail.com>
2019-11-14 11:03:38 +0200

    moved thread spawning from client to kernel

--- clients/terminal/src/terminal.cpp
+++ clients/terminal/src/terminal.cpp
@@ -3,25 +3,16 @@
 #include "common.h"
 #include "client_api.h"
 
-static callback_t g_transmitter;
-static std::thread g_wait_cmd_thread;
-
-static void waitCmd()
+void spawnClient(callback_t transmitter)
 {
-    while (1)
-    {
-        std::string cmd;
+    std::string cmd;
 
+    do
+    {
         std::cout << "nano_daw shell> ";
         std::getline(std::cin, cmd);
 
-        std::string api_response = g_transmitter(cmd);
+        std::string api_response = transmitter(cmd);
         std::cout << api_response << std::endl;
-    }
-}
-
-void spawnClient(callback_t transmitter)
-{
-    g_transmitter = transmitter;
-    g_wait_cmd_thread = std::thread(waitCmd);
+    } while (cmd != "quit");
 }
