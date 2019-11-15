c8c791cc65989ee64a51aa349cee4d684878e5c5
Belkevich <aleksey.belkevich@gmail.com>
2019-11-16 01:15:29 +0200

    added log entries

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -8,16 +8,23 @@ Session *g_session = nullptr;
 
 int main(int argc, char **argv)
 {
+    loguru::g_stderr_verbosity = loguru::Verbosity_OFF;
+    loguru::add_file("nano_daw.log", loguru::Append, loguru::Verbosity_MAX);
+
     if (argc != 2)
     {
+        LOG_F(ERROR, "Cannot find client in cmd line args");
         return 1;
     }
 
+    LOG_F(INFO, "Starting initializing client API with client %s", argv[1]);
     if (ClientAPI::initAPI(argv[1]) != 0)
     {
+        LOG_F(ERROR, "Cannot init api");
         return 2;
     }
 
+    LOG_F(INFO, "Starting wait thread");
     while (g_working)
     {
 		std::this_thread::sleep_for(std::chrono::milliseconds(1));
