08cd70417845f9ab251d82bf89ca2ddc7e22a5d4
Belkevich <aleksey.belkevich@gmail.com>
2019-11-21 08:37:09 +0200

    added error and info logs

--- kernel/src/items_manager.cpp
+++ kernel/src/items_manager.cpp
@@ -20,6 +20,9 @@ namespace ItemsManager
 		Effect *effect = new Effect(name, params);
 		id_t id = genUniqueId();
 
+        LOG_F(INFO, "Created effect with id: '%d' name: '%s' params: '%s'",
+                    id, name.c_str(), params.c_str());
+
 		auto rec = std::make_pair(id, effect);
 		g_effects.insert(rec);
 
@@ -28,20 +31,27 @@ namespace ItemsManager
 
 	id_t createAudio(std::string path)
 	{
+        LOG_F(INFO, "Loading audio from path: %s", path.c_str());
+
         CodecManager::CodecInfo codec_info;
         if (CodecManager::getCodec("pure_wave.dll", codec_info) != 0)
         {
+            LOG_F(ERROR, "Cannot load codec 'pure_wave.dll'");
             return 0;
         }
 
         CodecFileInfo codec_file_info;
         if (codec_info.load_file_proc(codec_file_info, path) != 0)
         {
+            LOG_F(ERROR, "Cannot load file content");
             return 0;
         }
 
 		Audio *audio = new Audio(path, codec_file_info.buffers[0], codec_file_info.samples_per_channel);
 		id_t id = genUniqueId();
+
+        LOG_F(INFO, "Created audio with id: '%d' path: '%s'",
+                    id, path.c_str());
 
 		auto rec = std::make_pair(id, audio);
 		g_audios.insert(rec);
@@ -51,13 +61,18 @@ namespace ItemsManager
 
 	id_t createFragment(id_t linked_audio)
 	{
+        LOG_F(INFO, "Creating fragment with linked audio %d", linked_audio);
+
 		if (!getAudio(linked_audio))
 		{
+            LOG_F(ERROR, "Cannot find linked audio");
 			return 0;
 		}
 
 		Fragment *fragment = new Fragment(linked_audio);
 		id_t id = genUniqueId();
+
+        LOG_F(INFO, "Created fragment id: '%d' audio: '%d'", id, linked_audio);
 
 		auto rec = std::make_pair(id, fragment);
 		g_fragments.insert(rec);
@@ -69,6 +84,8 @@ namespace ItemsManager
 	{
 		Track *track = new Track(name);
 		id_t id = genUniqueId();
+
+        LOG_F(INFO, "Created track id: '%d' name: '%d'", id, name);
 
 		auto rec = std::make_pair(id, track);
 		g_tracks.insert(rec);
@@ -118,53 +135,73 @@ namespace ItemsManager
 
 	bool removeEffect(id_t id)
 	{
+        LOG_F(INFO, "Removing track id: '%d'", id);
+
 		if (g_effects.find(id) == g_effects.end())
 		{
+            LOG_F(ERROR, "Cannot find track id");
 			return false;
 		}
 
 		delete g_effects.at(id);
-
 		g_effects.erase(id);
+
+        LOG_F(INFO, "Track removed");
+
 		return true;
 	}
 
 	bool removeAudio(id_t id)
 	{
+        LOG_F(INFO, "Removing audio id: '%d'", id);
+
 		if (g_audios.find(id) == g_audios.end())
 		{
+            LOG_F(ERROR, "Cannot find audio id");
 			return false;
 		}
 
 		delete g_audios.at(id);
-
 		g_audios.erase(id);
+
+        LOG_F(INFO, "Track removed");
+
 		return true;
 	}
 
 	bool removeFragment(id_t id)
 	{
+        LOG_F(INFO, "Removing fragment id: '%d'", id);
+
 		if (g_fragments.find(id) == g_fragments.end())
 		{
+            LOG_F(ERROR, "Cannot find fragment id");
 			return false;
 		}
 
 		delete g_fragments.at(id);
-
 		g_fragments.erase(id);
+
+        LOG_F(INFO, "Fragment removed");
+
 		return true;
 	}
 
 	bool removeTrack(id_t id)
 	{
+        LOG_F(INFO, "Removing track id: '%d'", id);
+
 		if (g_tracks.find(id) == g_tracks.end())
 		{
+            LOG_F(ERROR, "Cannot find track id");
 			return false;
 		}
 
 		delete g_tracks.at(id);
-
 		g_tracks.erase(id);
+
+        LOG_F(INFO, "Track removed");
+
 		return true;
 	}
 
